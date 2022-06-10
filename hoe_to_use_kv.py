a89f21c88a37d451ebaf2a5920aab2bbf5b09ff6
Marius Survila <mariussurvila@Mariuss-MBP.lan>
2022-06-10 19:52:50 +0300

    initialized git repository

---
+++ hoe_to_use_kv.py
@@ -0,0 +1,50 @@
+
+# code how to use .kv file in kivy
+ 
+# import kivy module
+import kivy
+import requests
+import json
+import re
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
+from kivy.uix.widget import Widget
+from kivy.app import App
+from kivy.properties import ObjectProperty
+
+Builder.load_file('kvfile.kv')
+
+class MyLayout(Widget):
+	
+    def circle(self):
+        response_API = requests.get('https://www.circlek.lt/privatiems/degalu-kainos')
+		# print(response_API.status_code)
+        data = response_API.text
+
+        kaina = re.findall(r"[-+]?\d*\.\d+", data)
+        circle_95_miles = kaina[2]
+
+        kainos_kitos = re.findall(r"[-+]?\d*\,\d+", data)
+        circle_95_miles_plus = kainos_kitos[0]
+        circle_98_miles_plus = kainos_kitos[1]
+        circle_d_miles = kainos_kitos[2]
+        circle_d_miles_plus = kainos_kitos[3]
+        circle_dz = kainos_kitos[4]
+        circle_lng = kainos_kitos[5]
+        print(circle_95_miles,circle_95_miles_plus,circle_98_miles_plus,circle_d_miles, circle_d_miles_plus, circle_dz, circle_lng)
+
+
+
+ 
+
+class kvfileApp(App):
+    def build(self):
+        return MyLayout()
+
+
+
+
+if __name__ == '__main__': 
+    kv = kvfileApp()
+    kv.run()
